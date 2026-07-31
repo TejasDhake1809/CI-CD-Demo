daf9f442347264d51be4e4c406a266f03fd93590
tejasdhake <tejasdhake04@gmail.com>
2026-07-31 04:25:38 +0000

    Initial Commit

---
+++ src/main/java/com/example/firstcd/FirstcdApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstcd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstcdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstcdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/firstcd/FirstcdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstcd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstcdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
